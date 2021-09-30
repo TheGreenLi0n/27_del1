c769626a05d1b2861107944d582b9b4fc2bd6ec6
Martin Wenzel <s203867@student.dtu.dk>
2021-09-30 13:51:11 +0200

    Player class, with update, get functions

--- 27_del1/src/Models/Player.java
+++ 27_del1/src/Models/Player.java
@@ -1,4 +1,25 @@
 package Models;
 
 public class Player {
+    private int score;
+    private boolean win;
+    private int turn;
+
+    public Player(){
+        score = 0;
+        win = false;
+        turn = 0;
+    }
+    public void updateScore(int x){
+        score += x;
+    }
+    public void updateWin(){
+        win = !win;
+    }
+    public int getScore(){
+        return score;
+    }
+    public int getTurn(){
+        return turn;
+    }
 }
