fb4725e35be3b760f5c63527e2901f77891ec2d4
SimonBruun <bsb.bruun@hotmail.com>
2021-09-30 17:44:09 +0200

    Tilføjet score metoder og lidt kommentarer.

--- 27_del1/src/Models/Player.java
+++ 27_del1/src/Models/Player.java
@@ -1,4 +1,20 @@
 package Models;
 
 public class Player {
+
+    //Erklærer en privat int "Score" med startværdien nul.
+    private int Score = 0;
+    Break = Die;
+    //Sætter en score til inten s for "Score" og får en score gennem getScore() som returner den nuværende værdi af scoren.
+    public void setScore(int s){
+        Score = s;
+    }
+    public int getScore() {
+        return Score;
+
+    }
+    public void addUp(int s){
+        Score += s;
+    }
+
 }
