56eee2b1569bdbcff989505ec971e6662e9a72fa
Martin Wenzel <s203867@student.dtu.dk>
2021-09-30 13:59:45 +0200

    Player class, with update, get functions

--- 27_del1/src/Models/Player.java
+++ 27_del1/src/Models/Player.java
@@ -4,13 +4,15 @@ public class Player {
     private int score;
     private boolean win;
     private int turn;
-
+    // constructor for player object
     public Player(){
         score = 0;
         win = false;
         turn = 0;
     }
+    // for updating score
     public void updateScore(int x){
+
         score += x;
     }
     public void updateWin(){
